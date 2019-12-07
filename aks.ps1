2b6ce16316dedc1a33746baedd68474a8321673a
akshay4932 <52039278+akshay4932@users.noreply.github.com>
2019-12-07 02:26:06 +0000

    Add files via upload

---
+++ aks.ps1
@@ -0,0 +1,23 @@
+﻿az login
+
+az acr login --name itrain0112
+
+
+#set image
+
+docker image ls
+#tag image for acr
+
+docker tag akshaymvcapp itrain0112.azurecr.io/akshaymvcapp:v1
+#push the image
+
+docker push itrain0112.azurecr.io/akshaymvcapp:v1
+
+
+az aks get-credentials --resource-group RG-ASP --name aks-itrain-0112
+
+kubectl get nodes
+
+kubectl apply -f D:\itrain\sample.yaml
+
+kubectl get service akshaymvcapp --watch
